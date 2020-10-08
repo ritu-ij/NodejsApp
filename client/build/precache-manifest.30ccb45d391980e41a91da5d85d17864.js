3ef8f06ef1b0090b968997a0624e3197ed7c9dfc
ritu.s <ritu.s@endurance.com>
2020-10-08 14:20:31 +0530

    Initial commit to our app

---
+++ client/build/precache-manifest.30ccb45d391980e41a91da5d85d17864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0513c6bcd97f7595a5a81b19064f6a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a52a1d2fc2cda7e97f5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd3d21fb6dce97b98a08",
+    "url": "/static/js/2.6f1ab550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f1ab550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a52a1d2fc2cda7e97f5",
+    "url": "/static/js/main.50e5c7f8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
